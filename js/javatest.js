921a0a6325eef089d89ddc6f4a651cd33470b7df
EpsilonXI <brandon-gonzalez-rivera@outlook.com>
2022-08-26 12:10:35 -0500

    clase 03

---
+++ js/javatest.js
@@ -0,0 +1,79 @@
+let global = "esta variable es global";
+
+function nombreFuncion(){
+    var local = "esta variable es local";
+    let global = "cambie la variable global";
+   // console.log("hola mundo");
+
+    
+    //console.log(local);
+}
+
+nombreFuncion();
+//console.log(global);
+
+var i = 20;
+
+console.log(i);
+/*
+for(let i=0; i< 10; i++){
+console.log(i);
+}
+*/
+{
+let i=100;
+console.log(i);
+}
+
+console.log(i);
+
+/*
+
+var a = 3;
+
+/* var b = 10;
+var bandera = true;
+var float = 10.55;
+
+console.log("a es un: " + typeof a);
+console.log(typeof b);
+console.log(typeof bandera);
+console.log(typeof float);
+*/
+/*
+a = [1,10,"Elemento 3", true];
+
+
+console.log(a[3]);
+
+console.log(a.length);
+
+console.log(a);
+
+a.push(20);
+
+console.log(a);
+
+a.pop();
+
+console.log(a);
+
+for(let i=0; i < a.length; i++){
+    console.log(a[i]);
+}
+
+nombreFuncion();
+
+var varFunction= function(){
+    console.log("variable function hola mundo");
+}
+
+varFunction();
+
+*/
+
+
+
+
+
+
